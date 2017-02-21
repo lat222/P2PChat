976ef092a47316d2d3102554bc5163866f8cdab1
Vince Messenger <Vincent.Messenger@icloud.com>
2017-02-21 11:45:24 -0700

    Cleaned up formatting of ReadThread case statement

--- ReadThread.java
+++ ReadThread.java
@@ -33,13 +33,84 @@ public class ReadThread extends Thread{
             // start receiving packets
             while(true) {
                 // receive packet
-                read_socket.receive(packet);
+                String temp;
+            DatagramPacket out;
+            String outMessage;
+            read_socket.receive(packet);
 
-                buffer = packet.getData();
-                
-                // todo switch statement
-                System.out.println("Just received packet from " + packet.getSocketAddress());
-                System.out.println("Text: " + new String(buffer));
+            buffer = packet.getData();
+            
+            // Message protocol handling
+            // TODO: format strings printed by System.out to user
+            String message = new String(buffer);
+            char messageType = message.charAt(0);
+            switch (messageType)
+            {
+                case 'J':
+                    // keeps the old next node to send later
+                    temp = Node.next_node;
+
+                    // set next_node to joining node and send connected message to that node
+                    Node.next_node = packet.getAddress().getHostName().toString();
+                    outMessage = String.format("C %s", temp);
+                    out = new DatagramPacket(outMessage.getBytes(),outMessage.getBytes().length,
+                    InetAddress.getByName(Node.next_node), port_num);
+                    read_socket.send(out);
+
+                case 'C':
+                    // get the substring of the message that contains the node's new next ip and set it to next
+                    Node.next_node = message.substring(2);
+
+                    // print message to user and send message on that the new user has joined the chat
+                    outMessage = String.format("M %s %s has joined the chat!!!", InetAddress.getLocalHost().getHostAddress(), Node.user_name);
+                    System.out.println(outMessage);
+                    out = new DatagramPacket(outMessage.getBytes(),outMessage.getBytes().length,
+                    InetAddress.getByName(Node.next_node), port_num);
+                    read_socket.send(out);
+
+                case 'L':
+                    String[] splitMessage = message.split(" ");
+
+                    // check message was not sent from the next ip and continue sending
+                    if(splitMessage[1].equals(Node.next_node) == false)
+                    {
+                      // print message
+                      System.out.println("Just received packet from " + packet.getSocketAddress());
+                      System.out.println("Text: " + new String(buffer));
+
+                      // send message
+                      out = new DatagramPacket(message.getBytes(),message.getBytes().length,
+                      InetAddress.getByName(Node.next_node), port_num);
+                      read_socket.send(out);
+                    }
+
+                    // message received at node that sent it, remove node from p2pchat by setting it's next to an empty string 
+                    else if(splitMessage[1].equals(InetAddress.getLocalHost().getHostAddress()) == true)
+                    {
+                      Node.next_node = "";
+                    }
+
+                    // message was sent from the next ip, so set next to it's next
+                    else
+                    {
+                      temp = Node.next_node;
+                      Node.next_node = splitMessage[2];
+                      out = new DatagramPacket(message.getBytes(),message.getBytes().length,
+                      InetAddress.getByName(temp), port_num);
+                      read_socket.send(out);
+                    }
+                    
+                case 'M':
+                    // check message was not sent from the local ip and continue sending
+                    if(message.split(" ")[1].equals(InetAddress.getLocalHost().getHostAddress()) == false)
+                    {
+                      System.out.println("Just received packet from " + packet.getSocketAddress());
+                      System.out.println("Text: " + message);
+                      out = new DatagramPacket(message.getBytes(),message.getBytes().length,
+                      InetAddress.getByName(Node.next_node), port_num);
+                      read_socket.send(out);
+                    } 
+                }
 
                 // this code sets the next_node value to the IP that sent packet
                 Node.next_node = packet.getAddress().getHostName().toString();
