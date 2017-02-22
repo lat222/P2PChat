07edcdbcfb2039ad1940a76e95b9b14ebf808fca
Vince Messenger <Vincent.Messenger@icloud.com>
2017-02-22 09:19:40 -0700

    we got a working p2p chat app

--- ReadThread.java
+++ ReadThread.java
@@ -1,6 +1,5 @@
 import java.net.*;
 import java.io.*;
-
 /**
 * Vincent Messenger, Thomas Back, Leia Trice
 * 
@@ -15,11 +14,11 @@ public class ReadThread extends Thread{
    
    @Override
    public void run() {
-
         // create stuffs
         DatagramPacket out;
         String temp;
         String outMessage;
+        Boolean running = true;
        
        // try to create io streams
        try(
@@ -39,7 +38,7 @@ public class ReadThread extends Thread{
             }
             
             // start receiving packets
-            while(true) {
+            while(running) {
                 byte[] buffer = new byte[1024];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 
@@ -48,17 +47,15 @@ public class ReadThread extends Thread{
                 buffer = packet.getData();
                 
                 // Message protocol handling switch statement
-                // TODO: format strings printed by System.out to user
                 String message = new String(buffer);
                 char messageType = message.charAt(0);
                 switch (messageType) {
-
                     case 'J':
                         // keeps the old next node to send later
                         temp = Node.next_node;
 
                         // set next_node to joining node and send connected message to that node
-                        Node.next_node = packet.getAddress().getHostName().toString();
+                        Node.next_node = packet.getAddress().getHostAddress().toString();
                         outMessage = String.format("C %s", temp);
                         out = new DatagramPacket(outMessage.getBytes(), outMessage.getBytes().length,
                             InetAddress.getByName(Node.next_node), port_num);
@@ -68,7 +65,7 @@ public class ReadThread extends Thread{
 
                     case 'C':
                         // get the substring of the message that contains the node's new next ip and set it to next
-                        Node.next_node = message.substring(2);
+                        Node.next_node = message.substring(2).trim();
 
                         // print message to user and send message on that the new user has joined the chat
                         outMessage = String.format("M %s %s has joined the chat!!!", InetAddress.getLocalHost().getHostAddress(), Node.user_name);
@@ -86,11 +83,12 @@ public class ReadThread extends Thread{
                         String[] splitLeaveMessage = message.split(" ");
 
                         // check message was not sent from the next ip and continue sending
-                        if(splitLeaveMessage[1].equals(Node.next_node) == false)
+                        Boolean same = splitLeaveMessage[1].equals(Node.next_node);
+
+                        if(!same && !splitLeaveMessage[1].equals(InetAddress.getLocalHost().getHostAddress().toString()))
                         {
                             // print message
                             print_subArray(splitLeaveMessage,3);
-
                             // send message
                             out = new DatagramPacket(message.getBytes(),message.getBytes().length,
                                 InetAddress.getByName(Node.next_node), port_num);
@@ -98,16 +96,20 @@ public class ReadThread extends Thread{
                         }
 
                         // message received at node that sent it, remove node from p2pchat by setting it's next to an empty string 
-                        else if(splitLeaveMessage[1].equals(InetAddress.getLocalHost().getHostAddress()) == true)
+                        else if(splitLeaveMessage[1].equals(InetAddress.getLocalHost().getHostAddress().toString()))
                         {
                             Node.next_node = "";
+
+                            // close socket
+                            read_socket.close();
+                            running = false;
                         }
 
                         // message was sent from the next ip, so set next to it's next
                         else
                         {
+                            // save old next_node
                             temp = Node.next_node;
-                            Node.next_node = splitLeaveMessage[2];
 
                             // print message
                             print_subArray(splitLeaveMessage,3);
@@ -116,6 +118,9 @@ public class ReadThread extends Thread{
                             out = new DatagramPacket(message.getBytes(),message.getBytes().length,
                                 InetAddress.getByName(temp), port_num);
                             read_socket.send(out);
+
+                            // switch next_node to senders next node
+                            Node.next_node = splitLeaveMessage[2];
                         }
 
                         break;
@@ -133,21 +138,15 @@ public class ReadThread extends Thread{
                         }
 
                         break; 
+                        
                     }
 
-                // this code sets the next_node value to the IP that sent packet
-                //Node.next_node = packet.getAddress().getHostName().toString();
-                //System.out.println("next_node: " + Node.next_node);
-                
-                // send packet back
-                //read_socket.send(packet);
             }
        
        } catch (IOException e) {
            e.printStackTrace();
        }
    }
-
     void print_subArray(String[] stringsToPrint, int index){
         for(int i = index; i<stringsToPrint.length; i++)
         {
--- WriteThread.java
+++ WriteThread.java
@@ -20,6 +20,7 @@ public class WriteThread extends Thread{
         // create stuffs
         DatagramPacket packet = null;
         String inString, toSend;
+        Boolean running = true;
 
        
         // try to create io streams
@@ -30,21 +31,38 @@ public class WriteThread extends Thread{
                 BufferedReader stdin =  new BufferedReader(new InputStreamReader(System.in));
 
                 // start listening for keyboard input
-                while((inString = stdin.readLine()) != null){
+                while(running){
+                    // read from keyboard
+                    inString = stdin.readLine();
 
                     // check what type of message user is trying to send, build message with protocol
-                    if (inString == "L"){
-                        toSend = "Fake";
-                        //
-                    }
-                    else {
-                        toSend = "M " + InetAddress.getLocalHost().getHostAddress().toString() + " " + inString;
-                    }
-                    // send out packet
-                    packet = new DatagramPacket(toSend.getBytes(), toSend.getBytes().length, InetAddress.getByName(Node.next_node), Node.send_port);
-                    write_socket.send(packet);
+                    // there are only 2 message types the user would write: message to leave and just
+                    // just a regular message
+                    if (inString.equals("L")){
 
+                        // message structure "L ip_address next_ip_address message_with_username"
+                        toSend = "L " + InetAddress.getLocalHost().getHostAddress().toString() +
+                            " " + Node.next_node + " " + Node.user_name + " is leaving.";
+
+                        // send out packet
+                        packet = new DatagramPacket(toSend.getBytes(), toSend.getBytes().length, InetAddress.getByName(Node.next_node), Node.send_port);
+                        write_socket.send(packet);
+                        write_socket.close();
+                        running = false;
+                    }
+                    else{
+                        // message structure "M ip_address message_with_username"
+                        toSend = "M " + InetAddress.getLocalHost().getHostAddress().toString() + " " + Node.user_name + " says: " + inString;
+
+                        // send out packet
+                        packet = new DatagramPacket(toSend.getBytes(), toSend.getBytes().length, InetAddress.getByName(Node.next_node), Node.send_port);
+                        write_socket.send(packet);
+                    }
+
+                    // new line for formatting
+                    System.out.print("\n");
                 }
+
         
         } catch (IOException e) {
             e.printStackTrace();
